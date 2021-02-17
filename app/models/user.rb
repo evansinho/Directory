aa57e283c7298943d02ed3ca6ec0945e69e9d532
evanson <eigiri@uplandsoftware.com>
2021-02-17 22:05:43 +0100

    create model/controller/view for users

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :name, presence: true, length: { in: 3..20 }
+  validates :phone, presence: true
+  validates :status, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :email, presence: true, length: { maximum: 244 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+end
